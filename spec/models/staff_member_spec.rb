a904ef0934bebd610a37632c2ffbfa857daa2ea4
nottak0409 <nottak0409@gmail.com>
2023-01-15 18:12:01 +0900

    [add]staff_membersのmodelを作成

--- spec/models/staff_member_spec.rb
+++ spec/models/staff_member_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe StaffMember, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#password=" do
+    example "文字列を与えると、hashed_passwordは60文字の文字列になる" do
+      member = StaffMember.new
+      member.password = "baukis"
+      expect(member.hashed_password).to be_kind_of(String)
+      expect(member.hashed_password.size).to eq(60)
+    end
+    
+    example "文字列がnilの場合、nilが返る" do
+      member = StaffMember.new
+      member.password = nil
+      expect(member.hashed_password).to be_nil
+    end
+  end
 end
