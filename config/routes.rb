a6d3fdc16b8b82da9ce28ab696138c97c6d72f1d
nottak0409 <nottak0409@gmail.com>
2023-01-19 20:07:44 +0900

    [add]routeの更新

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,15 @@ Rails.application.routes.draw do
   namespace :staff do
     root "top#index"
     get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+    resource :session, only: [ :create, :destroy ]
+    resource :account, except: [ :new, :create, :destroy ]
   end
 
   namespace :admin do
     root "top#index"
     get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
-    resources: :staff_members
+    resource :session, only: [ :create, :destroy ]
+    resources :staff_members
   end
 
   namespace :customer do
