1203c934dcbaee4c9d191e7e2ba34cf35c69478e
nottak0409 <nottak0409@gmail.com>
2023-01-31 18:01:14 +0900

    [add]バリデーションのrspec追加

--- app/models/concerns/string_normalizer.rb
+++ app/models/concerns/string_normalizer.rb
@@ -8,6 +8,10 @@ module StringNormalizer
     end
 
     def normalize_as_furigana(text)
-        NKF.nkf("-W -W -Z1 --katakana", text).strip if text
+        NKF.nkf("-W -w -Z1 --katakana", text).strip if text
+    end
+
+    def normalize_as_email(text)
+        NKF.nkf("-W -w -Z1", text).strip if text
     end
 end
