bf48c07c7839ad44ce1c62cfee21ea5e59e76b43
nottak0409 <nottak0409@gmail.com>
2023-02-15 21:45:04 +0900

    [add]emailholder作成

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,14 +1,7 @@
 class StaffMember < ApplicationRecord
-    include StringNormalizer
+    include EmailHolder
     include PersonalNameHolder
     has_many :events, class_name: "StaffEvent", dependent: :destroy
-
-    before_validation do
-        self.email = normalize_as_email(email)
-    end
-
-    validates :email, presence: true, "valid_email_2/email": true,
-        uniqueness: { case_sensitive: false }
 
     def password=(raw_password)
         if raw_password.kind_of?(String)
