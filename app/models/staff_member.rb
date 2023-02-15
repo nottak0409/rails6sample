e3aae6b5808625fdf06815d358b0570cec5a0a58
nottak0409 <nottak0409@gmail.com>
2023-02-15 21:48:22 +0900

    [add]passwordのモジュールを外だし

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,15 +1,8 @@
 class StaffMember < ApplicationRecord
     include EmailHolder
     include PersonalNameHolder
+    include PasswordHolder
     has_many :events, class_name: "StaffEvent", dependent: :destroy
-
-    def password=(raw_password)
-        if raw_password.kind_of?(String)
-            self.hashed_password = BCrypt::Password.create(raw_password)
-        elsif raw_password.nil?
-            self.hashed_password = nil
-        end
-    end
 
     def active?
         !suspended? && start_date <= Date.today &&
